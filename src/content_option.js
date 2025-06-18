7e3142f50dcbe0767c8e19c24d3d9814b2ae5ca8
DienDead <aniketdebu@hotmail.com>
2025-06-18 15:19:05 +0530

    portfolio

---
+++ src/content_option.js
@@ -0,0 +1,130 @@
+import "./"
+
+const logotext = "D3BU";
+const meta = {
+    title: "Aniket Mishra",
+    description: "I'm Aniket Mishra FrontEnd Developer",
+};
+
+const introdata = {
+    title: "I'm Aniket Mishra",
+    animated: {
+        first: "I like coding",
+        second: "I code to build websites",
+        third: "I develop Database & API based web applications",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://i.postimg.cc/MTzSYGbf/AM.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "'I'm a moderately skilled Web Developer with a wealth of diverse life and technical experience that I bring to every project.I follow form as well as function.I know that the aesthetics of effective design combined with application speed, security and reliability combine to create a great user experience.I am skilled in Server and Query Management.I get indulged in PVP online games Counter Strike & Valorant. ",
+};
+const worktimeline = [{
+        jobtitle: "Scrims Manager",
+        where: "Echoverse Esports (INDIA)",
+        date: "2024 (Present)",
+    },
+    {
+        jobtitle: "Full Stack Developer intern",
+        where: "Unified Mentor",
+        date: "2024",
+    },
+];
+
+const skills = [{
+        name: "Java",
+        value: 80,
+    },
+    {
+        name: "MongoDB",
+        value: 90,
+    },
+    {
+        name: "Javascript(Node and Express)",
+        value: 90,
+    },
+    {
+        name: "ReactJS",
+        value: 80,
+    },
+    {
+        name: "MySQL",
+        value: 80,
+    },
+    {
+        name: "SpringBoot",
+        value: 50,
+    },
+    {
+        name:"AWS",
+        value: 40,
+    },
+];
+
+const services = [{
+        title: "FrontEnd Design",
+        description: "",
+    },
+    {
+        title: "Web Apps",
+        description: "",
+    },
+    {
+        title: "Web Design",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://res.cloudinary.com/dktjhrnox/image/upload/v1750236560/Screenshot_2025-06-18_134934_zx2gmq.png",
+        description: "Web Chat Application",
+        link: "https://github.com/AryaSarkar01022004/React-Chat-App",
+    },
+    {
+        img: "https://res.cloudinary.com/dktjhrnox/image/upload/v1750237606/Screenshot_2025-06-18_143627_lonfgs.png",
+        description: "Movie's Trailer and Review Client",
+        link: "https://github.com/DienDead/m_clients",
+    },
+    {
+        img: "https://res.cloudinary.com/dktjhrnox/image/upload/v1750234266/Screenshot_2025-06-18_132546_nstdt0.png",
+        description: "WanderLust - Property Listing",
+        link: "https://github.com/DienDead/WanderLust",
+    },
+    {
+        img:"https://res.cloudinary.com/dktjhrnox/image/upload/v1750239964/Screenshot_2025-06-18_151521_i4nkc5.png",
+        description:"Brailer E-commerce",
+        link:"https://github.com/DienDead/Brailer-E-commerce",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "aniketdebu@hotmail.com",
+    YOUR_FONE: "7682966499",
+    description: "For any project ideas feel free to contact I am open to freelancing as well.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_68x0wfk",
+    YOUR_TEMPLATE_ID: "template_ajtssqq",
+    YOUR_USER_ID: "",
+};
+
+const socialprofils = {
+    github: "https://github.com/DienDead?tab=overview&from=2021-12-01&to=2021-12-31",
+    facebook: "https://www.facebook.com/share/JG2bMQEJ3AY1gSyT/?mibextid=qi2Omg",
+    linkedin: "https://www.linkedin.com/in/aniket-mishra-005150279/",
+    twitter: "https://x.com/diendead?t=G4kqnsyDzD4dcMenCDd4gA&s=08",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
